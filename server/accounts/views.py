690ce9c3238dd28a5cb24c673edcfcb5d39e4c56
postman17 <frompostal@gmail.com>
2018-10-29 17:59:28 +0500

    add register form

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from accounts.forms import AccountUserForm
+from .forms import AccountUserForm, RegisterUserForm
+from .models import AccountUser
 from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 
@@ -23,3 +24,27 @@ def accounts_login(request):
                 return redirect(success_url)
 
     return render(request, 'accounts/login.html', {'form': form})
+
+def accounts_register(request):
+    success_url = reverse_lazy('accounts:login')
+    form = RegisterUserForm()
+    if request.method == 'POST':
+        form = RegisterUserForm(data=request.POST)
+        if form.is_valid():
+            # Подскажите пожалуйста как правильно проверять наличие записи в базе(без обработчика ошибок)?
+            try:
+                db_request = AccountUser(username=form.cleaned_data['username'])
+            except Exception:
+                user = AccountUser(
+                    username=form.cleaned_data['username'],
+                    is_staff=True,
+                    is_superuser=False,
+                )
+                user.set_password(form.cleaned_data['password'])
+                user.save()
+                return redirect(success_url)
+            #Почему-то невыводятся ошибки
+            form.add_error(None, 'Пользователь существует!')
+            return render(request, 'accounts/register.html', {'form': form})
+
+    return render(request, 'accounts/register.html', {'form': form})
--- server/product/management/commands/fromfileintodb.py
+++ server/product/management/commands/fromfileintodb.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
 from product.models import Product
+from images.models import Image
 import json
+
 
 class Command(BaseCommand):
     help = 'Add data form file into database.'
+
     def handle(self, *args, **options):
         with open('server/data/data.json', encoding='utf-8') as file:
             data = json.load(file)
@@ -12,9 +15,11 @@ class Command(BaseCommand):
                 Product.objects.get(title=value['name'])
             self.stdout.write(self.style.SUCCESS('All records already exists'))
         except Exception:
+            image = Image.objects.get(title='default')
             for item in data['products']:
                 main = Product()
                 main.title = item['name']
                 main.snippet = item['content']
+                main.image = image
                 main.save()
             self.stdout.write(self.style.SUCCESS('Operation success!'))
