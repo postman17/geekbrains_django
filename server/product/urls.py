63447092ec1dcfbb742b6bc23e1fea5a27a34102
postman17 <frompostal@gmail.com>
2018-10-29 23:42:47 +0500

    add form product

--- server/product/urls.py
+++ server/product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from product.views import view_products, details
+from product.views import view_products, details, add_product
 
 app_name = 'product'
 urlpatterns = [
     path('', view_products, name='index'),
-    path('detail/<str:product_name>/<str:product_content>', details, name='detail'),
+    path('add', add_product, name='add'),
+    path('detail/<str:product_name>/<str:product_content>/<path:product_image>', details, name='detail'),
 ]
